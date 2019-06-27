1db8d05094b2f06ad8b3c0d8fca3cf5242c0e2c8
UBUSERV1 <ubuntu@ubuserv1.kyiv.epam.com>
2019-06-27 08:46:28 +0000

    working version

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-openssl genrsa -out key.pem 2048 && openssl rsa -in key.pem -outform PEM -pubout -out public.pem
+
+#remove old key and generate new one
+sudo rm -f key.pem public.pem public_header.pem 
+
+sudo openssl genrsa -out key.pem 2048 && openssl rsa -in key.pem -outform PEM -pubout -out public_header.pem
+
+sed -e '9d;1d' public_header.pem > public.pem
