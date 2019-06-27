fdeea1c781043e2704892578c84266749b257d8c
Roman Fedoryshyn <Roman_Fedoryshyn@epam.com>
2019-06-27 12:30:44 +0300

    provisioning added

--- main.tf
+++ main.tf
@@ -194,3 +194,38 @@ resource "aws_instance" "bastion" {
 #  }
 }
 
+
+#preparing inventory
+
+provisioner "local-exec" {
+
+  comand = "echo ${aws_instance.devtools.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.ci.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.qa.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.docker.private_ip} >> hosts"
+}
+
+
+
+#Installing ansible on bastion
+provisioner "remote-exec" {
+  
+  connection {
+    host     = "${aws_instance.bastion.public_ip}"
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("key.pem")}"
+  }
+
+  inline = [
+            "cd",
+            "sudo apt install python -y",
+            "sudo wget https://bootstrap.pypa.io/get-pip.py",
+            "sudo chmod +x get-pip.py",
+            "sudo python get-pip.py",
+            "sudo rm get-pip.py",
+            "sudo pip install ansible",
+            ]
+}
+
+#providing identity key to manage infrastucture
--- provisioning/main.tf
+++ provisioning/main.tf
@@ -1,10 +1,35 @@
-provisioner "file" {
-  source      = "ansible_install.sh"
-  destination = "/var/users/ubuntu"
 
+#preparing inventory
+
+provisioner "local-exec" {
+
+  comand = "echo ${aws_instance.devtools.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.ci.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.qa.private_ip} >> hosts"
+  #comand = "echo ${aws_instance.docker.private_ip} >> hosts"
+}
+
+
+
+#Installing ansible on bastion
+provisioner "remote-exec" {
+  
   connection {
+    host     = "${aws_instance.bastion.public_ip}"
     type     = "ssh"
     user     = "ubuntu"
-    private_key = "${file("privkey.ppk")}"
+    private_key = "${file("key.pem")}"
   }
+
+  inline = [
+            "cd",
+            "sudo apt install python -y",
+            "sudo wget https://bootstrap.pypa.io/get-pip.py",
+            "sudo chmod +x get-pip.py",
+            "sudo python get-pip.py",
+            "sudo rm get-pip.py",
+            "sudo pip install ansible",
+            ]
 }
+
+#providing identity key to manage infrastucture
