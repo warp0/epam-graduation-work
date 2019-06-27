ed24fa70786da32c2fd633c39ff0c8833ca47668
Roman Fedoryshyn <Roman_Fedoryshyn@epam.com>
2019-06-27 10:33:36 +0300

    security group protocol  6 (tcp)

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_security_group" "webserv" {
   ingress {
     from_port       = 80
     to_port         = 80
-    protocol        = "-1"
+    protocol        = "6"
     cidr_blocks     = ["0.0.0.0/0"] 
   }
   egress {
@@ -125,7 +125,7 @@ resource "aws_security_group" "ssh" {
   ingress {
     from_port       = 22
     to_port         = 22
-    protocol        = "-1"
+    protocol        = "6"
     cidr_blocks     = ["0.0.0.0/0"] 
   }
   egress {
